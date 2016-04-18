2852282c2c85eb775f0515a1fddb68ede45e32f5
byrnli <liam.byrne@abs.gov.au>
2016-04-18 15:45:49 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
+
+pcode = 2000
+html = scraperwiki.scrape("http://www.censusdata.abs.gov.au/census_services/getproduct/census/2011/quickstat/POA"+str(pcode))
 #
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
